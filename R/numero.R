e639ffe47b9556a13c6a28f1c7779227af301318
soniafattori <soniafattori.30@gmail.com>
2026-04-30 15:01:37 +0200

    Create numero.R

---
+++ R/numero.R
@@ -0,0 +1,12 @@
+#if else
+numero <- function(x){
+  if(x>0){
+    print("Questo numero è positivo, ciuco!")
+    }
+  else if(x<0) {
+    print("Questo numero è negativo, se non sai queste cose, torna a scuola")  
+    }
+  else {
+    print("Zero non è né negativo né positivo") 
+    }
+  }
